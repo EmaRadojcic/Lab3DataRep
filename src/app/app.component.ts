06f31522a4defdf8a470082b7a650d7594d009e5
EMA RADOJCIC - STUDENT <G00343805@gmit.ie>
2019-09-30 11:46:56 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MovieServiceService } from './Services/movie-service.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'movie-app';
-}
+    export class AppComponent{
+      title = 'movie-app';
+      MyMovies:any = [];
+
+      constructor(private movieService:MovieServiceService){
+        this.movieService.GetMovieData().subscribe((data)=>{
+          this.MyMovies = data.Search;
+          console.log(this.MyMovies);
+      })
+
+    // ngOnInit(){
+      //  this.movieService.GetMovieData().subscribe((data)=>{
+      //      this.MyMovies = data.Search;
+      //  });
+
+      }
+    }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule,MatCardModule,MatButtonModule,MatToolbarModule,MatExpansionModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
